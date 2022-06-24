eef5d155fdb6fd102cb70968f7ebc8021f7efaca
arzaaan789 <k190136@nu.edu.pk>
2022-06-25 01:20:05 +0500

    1st day

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Home() {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    height: 70px;
+    background-color: #090b13;
+  
+`
